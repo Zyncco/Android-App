f95ac48fe27ebdbf736cc281b6511daf450a2b31
Vilsol <vilsol2000@gmail.com>
2017-04-25 17:39:23 +0300

    Fix build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
-
-sed -i 's/versionCode [0-9]\+/versionCode '"$TRAVIS_BUILD_NUMBER"'/' app/build.gradle
-
-./gradlew publishApkRelease
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ];
+then
+	sed -i 's/versionCode [0-9]\+/versionCode '"$TRAVIS_BUILD_NUMBER"'/' app/build.gradle
+	./gradlew publishApkRelease
+fi
