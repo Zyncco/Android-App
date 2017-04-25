9b3fafab6673c2d413c5ffb1f23bae370e53b300
Vilsol <vilsol2000@gmail.com>
2017-04-25 18:28:05 +0300

    Fix build, deployment, firebase

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ];
 then
 	sed -i 's/versionCode [0-9]\+/versionCode '"$TRAVIS_BUILD_NUMBER"'/' app/build.gradle
